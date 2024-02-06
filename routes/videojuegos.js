560033aaf0328de25b3a3fed1cb629447c8686da
Alejandra <alejandra_santiago_95@hotmail.com>
2024-02-06 23:06:19 +0100

    COSIS

--- routes/videojuegos.js
+++ routes/videojuegos.js
@@ -17,6 +17,10 @@ router.get('/new', function(req, res, next) {
   res.render('videojuegos/new_videojuego', { title: 'New videogame' });
 });
 
+router.get('/:id', async(req, res) => {
+  
+});
+
 router.post('/new', async(req, res) => {
   let titulo = req.body.titulo;
   let consola = req.body.consola;
@@ -25,6 +29,20 @@ router.post('/new', async(req, res) => {
 
   res.redirect('/videojuegos');
 });
+
+async function get(id) {
+  try {
+    const database = client.db('db_comics');
+    const collection = database.collection('videojuegos');
+    const query = { _id: id };
+    var videojuegos = await collection.findOne(query).toArray();
+    console.log(videojuego);
+    return videojuego;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
 
 async function insert(titulo, consola) {
   try {
@@ -45,7 +63,6 @@ async function run() {
     try {
       const database = client.db('db_comics');
       const collection = database.collection('videojuegos');
-      // Query for a movie that has the title 'Back to the Future'
       const query = {};
       var videojuegos = await collection.find(query).toArray();
       console.log(videojuegos);
